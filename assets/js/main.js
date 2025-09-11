029bf39a167451264f79f02a5fabfa08bdd347bc
Cameron Scott <33464785+CScott96@users.noreply.github.com>
2025-09-11 13:56:03 +0100

    add initial functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,16 @@ const titleText = document.createElement("h1");
 titleText.innerText = "Odin Calculator";
 
 title.appendChild(titleText);
+
+function addition(num1, num2) {
+  console.log(num1 + num2);
+}
+function subtraction(num1, num2) {
+  console.log(num1 - num2);
+}
+function division(num1, num2) {
+  console.log(num1 / num2);
+}
+function multiplication(num1, num2) {
+  console.log(num1 * num2);
+}
