cacf04ae5813c9f30bd62764c6545c11188a376a
Cameron Scott <33464785+CScott96@users.noreply.github.com>
2025-09-11 14:02:53 +0100

    add operate function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const title = document.querySelector("#Title");
 const titleText = document.createElement("h1");
-titleText.innerText = "Odin Calculator";
 
+let num1, num2, operator;
+
+titleText.innerText = "Odin Calculator";
 title.appendChild(titleText);
 
 function addition(num1, num2) {
@@ -15,4 +17,23 @@ function division(num1, num2) {
 }
 function multiplication(num1, num2) {
   console.log(num1 * num2);
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      addition(num1, num2);
+      break;
+    case "-":
+      subtraction(num1, num2);
+      break;
+    case "*":
+      multiplication(num1, num2);
+      break;
+    case "/":
+      division(num1, num2);
+      break;
+    default:
+      console.log("no operator passed");
+  }
 }
